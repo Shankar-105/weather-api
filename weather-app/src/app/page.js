6bc248b89aecc74afbb2c72df8cabf577735daaf
Lekhya-18 <pittalekhya.24.cse@anits.edu.in>
2026-06-05 08:57:48 +0530

    Upgraded UI A/C to the Climate

--- weather-app/src/app/page.js
+++ weather-app/src/app/page.js
@@ -1,16 +1,28 @@
+"use client";
+
+import { useState } from "react";
 import SearchBar from "./components/SearchBar";
 import WeatherCard from "./components/WeatherCard";
-import "./globals.css";
 
 export default function Home() {
+  const [temp, setTemp] = useState(null);
+
   return (
-    <div className="app">
+    <div className={`app ${getWeatherClass(temp)}`}>
       <div className="overlay">
         <h1 className="title">🌤 Weather App</h1>
 
-        <SearchBar />
-        <WeatherCard />
+        <SearchBar setTemp={setTemp} />
+        <WeatherCard temp={temp} />
       </div>
     </div>
   );
+}
+
+// background logic to change the bg images according to the climate 
+function getWeatherClass(temp) {
+  if (!temp) return "default";
+  if (temp < 15) return "cold";
+  if (temp <= 28) return "normal";
+  return "hot";
 }
